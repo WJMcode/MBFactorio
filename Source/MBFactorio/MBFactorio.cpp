8809c94c19b7de627deae5b2cc90bdc0ce9f214b
DESKTOP-COA1C72\AR student <test@gmail.com>
2025-04-03 17:51:26 +0900

    - project init

---
+++ Source/MBFactorio/MBFactorio.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MBFactorio.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MBFactorio, "MBFactorio" );
