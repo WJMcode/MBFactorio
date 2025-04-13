c7653438707c0dd153468dc911db22c34ae0f11f
DESKTOP-7C0BQSS\gustj <gustjrajt@gmail.com>
2025-04-14 06:03:01 +0900

    - 누락코드 붙임

--- Source/MBFactorio/Struct/MBFStruct.h
+++ Source/MBFactorio/Struct/MBFStruct.h
@@ -126,6 +126,12 @@ public:
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
     FName BakedItem;
 
+
+    // 2025.04.13 유효성 검사 함수 (이윤정)
+    bool IsValid() const
+    {
+        return !ItemID.IsNone() && !ItemName.IsEmpty() && MaxCount > 0 && Image != nullptr;
+    }
     // 기본 생성자
     FItemData()
         : ItemID(FName("0")), MaxCount(0), Image(nullptr)
